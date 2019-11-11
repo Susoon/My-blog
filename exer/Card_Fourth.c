39eadad5115c5d1a4215f6d0b367725911c446b8
Susoon <tnghks9735@gmail.com>
2019-11-11 11:22:43 +0900

    update

--- exer/Card_Fourth.c
+++ exer/Card_Fourth.c
@@ -1,40 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int Find_Loca(int move)
+typedef struct node
 {
-	int ret = 0;
-	
-	return ret;
+	int index;
+	int num;
+	int tan_index;
+	struct node * next;
+}Node;
+
+int Find_Loca(Node** arr, int location, int move)
+{
+	if(arr[location] -> tan_index == -1)
+		return location + move;
+	Node * tmp = arr[location];
+	while(tmp -> num > move)
+	{
+		move -= tmp -> num;
+		location = tmp -> index;
+	}	
+
+	return location + move;
 }
 
-void sorting(int * arr, int n)
+void sorting(Node ** arr, int n)
 {
-	int location = 0;
+	int location = 1;
 	for(int i = 1; i <= n; i++)
 	{	
-		location = Find_Loca(i % (n - i + 1));	
+		location = Find_Loca(arr, location, i % (n - i + 1)) % n;
 
-		arr[location] = i;
+		arr[location] -> tan_index = i;
 	}
 }
 
 int main(void)
 {
 	int n, count = 1001;
-	int * arr;
-
+	Node ** arr; 
 	scanf("%d",&n);
 	
-	arr = (int*)calloc(n + 1, sizeof(int));
-
-	for(int i = 0; i <= n; i++) { arr[i] = i;}
+	arr = (Node**)malloc(sizeof(Node*) * n);
+	
+	for(int i = 1; i <= n; i++)
+	{
+		arr[i] = (Node*)malloc(sizeof(Node));
+		arr[i] -> index = i;
+		arr[i] -> tan_index = 0;
+		arr[i] -> num = -1;
+	}
+	
+	for(int i = 1; i < n; i++)
+	{
+		arr[i] -> next = arr[i + 1];
+	}
+	arr[n] -> next = arr[1];
 
 	sorting(arr, n);
 
 	for(int i = 1; i <= n; i++)
 	{
-		printf("%d ",arr[i]);
+		printf("%d ", arr[i] -> tan_index);
 	}
 	
 	printf("\n");
