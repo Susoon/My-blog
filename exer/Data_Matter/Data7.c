06a4c0df9cec4091c18c8eedbb6e126fdaaef76a
Susoon <tnghks9735@gmail.com>
2019-11-24 00:42:27 +0900

    update

--- exer/Data_Matter/Data7.c
+++ exer/Data_Matter/Data7.c
@@ -4,6 +4,7 @@
 
 int n1;
 int n2;
+
 int Dense_Case(char** strArr1, char ** strArr2, int len, int start1, int end1, int start2, int end2)
 {
 	int i = start1, j = start2, cmp, count = 0;
@@ -66,36 +67,29 @@ int Normal_Case(char ** strArr1, char ** strArr2, int len, int start1, int end1,
 int Recursive_Counting(char ** strArr1, char** strArr2, int len, int start1, int end1, int start2, int end2)
 {
 	int count = 0, middle = (start1 + end1) / 2, tmp_s, tmp_m, tmp_e;
-	static int height = 0;
-	if(height == 10)
+	if(end1 - start1 < 1000)
 	{
 		if((end2 - start2) / (end1 - start1) < 50)
 			return Dense_Case(strArr1, strArr2, len, start1, end1, start2, end2);
 		else
 			return Normal_Case(strArr1, strArr2, len, start1, end1, start2, end2);
 	}
-	height++;
 	
 	tmp_s = Binary_Search(strArr2, strArr1[start1], len, start2, end2);
 	tmp_m = Binary_Search(strArr2, strArr1[middle], len, start2, end2);
 	tmp_e = Binary_Search(strArr2, strArr1[end1], len, start2, end2);
-
 	
 	count += Recursive_Counting(strArr1, strArr2, len, start1, middle, tmp_s, tmp_m);
 	count += Recursive_Counting(strArr1, strArr2, len, middle + 1, end1, tmp_m, tmp_e);
-	height--;
 
 	return count;
 }
 
 int countCommonStrings(char** strArr1, char ** strArr2)
 {
-	int len = strlen(strArr1[0]), start = 0, end = n2;
-	start = Binary_Search(strArr2, strArr1[0], len, 0, n2);
+	int len = strlen(strArr1[0]);
 	
-	end = Binary_Search(strArr2, strArr1[n1 - 1], len, 0, n2);
-	
-	return Recursive_Counting(strArr1, strArr2, len, 0, n1, start, end);
+	return Recursive_Counting(strArr1, strArr2, len, 0, n1 - 1, 0, n2 - 1);
 }
 		
 int main(void)
